8cd10fbbf61bc280658e78bfd93ace6ad8bd971d
Yeison Tapia <yftapial@ut.edu.co>
2019-11-19 17:10:11 -0500

    + Add repositories standard implements

---
+++ Repositories/Cache/CacheQuoteDecorator.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Modules\Iquote\Repositories\Cache;
+
+use Modules\Iquote\Repositories\QuoteRepository;
+use Modules\Core\Repositories\Cache\BaseCacheDecorator;
+
+class CacheQuoteDecorator extends BaseCacheDecorator implements QuoteRepository
+{
+  public function __construct(QuoteRepository $quote)
+  {
+    parent::__construct();
+    $this->entityName = 'iquote.quotes';
+    $this->repository = $quote;
+  }
+
+  /**
+   * List or resources
+   *
+   * @return collection
+   */
+  public function getItemsBy($params)
+  {
+    return $this->remember(function () use ($params) {
+      return $this->repository->getItemsBy($params);
+    });
+  }
+
+  /**
+   * find a resource by id or slug
+   *
+   * @return object
+   */
+  public function getItem($criteria, $params)
+  {
+    return $this->remember(function () use ($criteria, $params) {
+      return $this->repository->getItem($criteria, $params);
+    });
+  }
+
+  /**
+   * create a resource
+   *
+   * @return mixed
+   */
+  public function create($data)
+  {
+    $this->clearCache();
+
+    return $this->repository->create($data);
+  }
+
+  /**
+   * update a resource
+   *
+   * @return mixed
+   */
+  public function updateBy($criteria, $data, $params)
+  {
+    $this->clearCache();
+
+    return $this->repository->updateBy($criteria, $data, $params);
+  }
+
+  /**
+   * destroy a resource
+   *
+   * @return mixed
+   */
+  public function deleteBy($criteria, $params)
+  {
+    $this->clearCache();
+
+    return $this->repository->deleteBy($criteria, $params);
+  }
+}
