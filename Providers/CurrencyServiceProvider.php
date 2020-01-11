9364a8620623d94d11430109f3399c7c58e0a80f
YeisonTapia <yftapial@ut.edu.co>
2020-01-11 11:50:38 -0500

    Add Config helper currency for user like facade in all app.

---
+++ Providers/CurrencyServiceProvider.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Modules\Iquote\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Modules\Iquote\Helpers\Currency;
+
+class CurrencyServiceProvider extends ServiceProvider
+{
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = true;
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->bind('currency',function(){
+          return new Currency;
+        });
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return [];
+    }
+}
