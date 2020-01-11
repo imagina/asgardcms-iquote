42efa38b53c2254dc592f7a348c0b6b289ec19f1
YeisonTapia <yftapial@ut.edu.co>
2020-01-11 11:45:38 -0500

    Update Permissions.

--- Config/permissions.php
+++ Config/permissions.php
@@ -31,7 +31,14 @@ return [
         'edit' => 'iquote::quotes.edit resource',
         'destroy' => 'iquote::quotes.destroy resource',
     ],
+    'iquote.currencies' => [
+        'index' => 'iquote::currencies.list resource',
+        'create' => 'iquote::currencies.create resource',
+        'edit' => 'iquote::currencies.edit resource',
+        'destroy' => 'iquote::currencies.destroy resource',
+    ],
 // append
+
 
 
 
