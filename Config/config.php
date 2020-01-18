8eaad030cd6bf7b593f37e599ef6fa6846bd81a9
YeisonTapia <yftapial@ut.edu.co>
2020-01-18 10:55:55 -0500

    Add configs for fake Fields.

--- Config/config.php
+++ Config/config.php
@@ -1,5 +1,34 @@
 <?php
 
 return [
-    'name' => 'Iquote'
+    'name' => 'Iquote',
+    'fields' => [
+        'quote' => [
+            'name' => [
+                'className' => 'col-xs-12 col-md-6',
+                'value' => '',
+                'type' => 'input',
+                'rules' => 'required',
+                'props' => [
+                    'label' => trans('iquote::iquotes.config.fake.name')
+                ]
+            ],
+            'categories' => [
+              'className' => 'col-xs-12 col-md-6',
+              'value' => null,
+              'type' => 'select',
+              'loadOptions' => [
+                'apiRoute' => 'apiRoutes.qblog.categories',
+                'select' => json_decode("{\"label\":\"title\",\"id\":\"id\"}"),
+              ],
+              'props' => [
+                'useChips' => false,
+                'label' => trans('iquote::iquotes.config.fake.categories')
+              ]
+            ]
+        ],
+        'product' => [],
+        'characteristic' => [],
+        'package' => [],
+    ]
 ];
